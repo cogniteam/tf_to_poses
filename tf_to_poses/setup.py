5ee9e55d08f676c0e9c39f3c0d1b97e58edc3fc6
Yakir <yakirhuri21@gmail.com>
2024-01-08 09:21:48 +0000

    first commit

---
+++ tf_to_poses/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+package_name = 'tf_to_poses'
+from glob import glob
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='yakirhuri21@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'tf_to_poses_node = tf_to_poses.tf_to_poses_node:main'
+        ],
+    },
+)
---
+++ tf_to_poses/tf_to_poses/tf_to_poses_node.py
@@ -0,0 +1,108 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped, TransformStamped
+from std_msgs.msg import Float64
+import tf2_ros
+from tf2_ros import TransformListener
+
+
+
+class MyTransformNode(Node):
+    def __init__(self):
+        super().__init__('my_transform_node')
+
+        self.declare_parameter('base_frame', 'base_link')
+        self.base_frame = self.get_parameter('base_frame').get_parameter_value().string_value
+        
+        self.declare_parameter('laser_frame', 'laser')
+        self.laser_frame = self.get_parameter('laser_frame').get_parameter_value().string_value
+
+        self.declare_parameter('camera_frame', 'camera_link')
+        self.camera_frame = self.get_parameter('camera_frame').get_parameter_value().string_value
+
+        self.declare_parameter('global_frame', 'map')
+        self.global_frame = self.get_parameter('global_frame').get_parameter_value().string_value
+
+        self.declare_parameter('rate', 5.0)
+        self.rate = self.get_parameter('rate').get_parameter_value().double_value
+
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+
+        self.pose_publisher_base = self.create_publisher(
+            PoseStamped, '/tf_'+self.base_frame, 10)
+        self.pose_publisher_laser = self.create_publisher(
+            PoseStamped, '/tf_'+self.laser_frame, 10)
+        self.pose_publisher_camera = self.create_publisher(
+            PoseStamped, '/tf_'+self.camera_frame, 10)
+
+      
+          
+
+        # Parameters for timer rates
+        self.declare_parameter('map_to_base_rate', self.rate)
+        self.declare_parameter('map_to_laser_rate', self.rate)
+        self.declare_parameter('map_to_camera_rate',self.rate)
+
+        # Timers
+        self.map_to_base_timer = self.create_timer(
+            self.get_parameter('map_to_base_rate').value,
+            self.map_to_base_callback
+        )
+
+        self.map_to_laser_timer = self.create_timer(
+            self.get_parameter('map_to_laser_rate').value,
+            self.map_to_laser_callback
+        )
+
+        self.map_to_camera_timer = self.create_timer(
+            self.get_parameter('map_to_camera_rate').value,
+            self.map_to_camera_callback
+        )
+
+    def map_to_base_callback(self):
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                self.base_frame, self.global_frame, rclpy.time.Time())
+            pose_msg = self.create_pose_msg(transform)
+            self.pose_publisher_base.publish(pose_msg)
+        except Exception as e:
+            self.get_logger().error(str(e))
+
+    def map_to_laser_callback(self):
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                self.laser_frame, self.global_frame, rclpy.time.Time())
+            pose_msg = self.create_pose_msg(transform)
+            self.pose_publisher_laser.publish(pose_msg)
+        except Exception as e:
+            self.get_logger().error(str(e))
+
+    def map_to_camera_callback(self):
+        try:
+            transform = self.tf_buffer.lookup_transform(
+                self.camera_frame, self.global_frame, rclpy.time.Time())
+            pose_msg = self.create_pose_msg(transform)
+            self.pose_publisher_camera.publish(pose_msg)
+        except Exception as e:
+            self.get_logger().error(str(e))
+
+    def create_pose_msg(self, transform):
+        pose_msg = PoseStamped()
+        pose_msg.header.frame_id = self.global_frame
+        pose_msg.header.stamp = rclpy.time.Time().to_msg()
+        pose_msg.pose.position.x = transform.transform.translation.x
+        pose_msg.pose.position.y = transform.transform.translation.y
+        pose_msg.pose.position.z = transform.transform.translation.z
+        pose_msg.pose.orientation = transform.transform.rotation
+        return pose_msg
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyTransformNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
